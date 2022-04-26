caded935517e2d8984f05b8c4a7d712a15f1a574
cwj851 <2354073526@qq.com>
2022-04-26 20:21:17 +0800

    updata

---
+++ resources/for_update/OutdateFiles.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: TonyJiangWJ
+ * @Date: 2020-05-01 12:28:02
+ * @Last Modified by: TonyJiangWJ
+ * @Last Modified time: 2020-12-30 20:08:59
+ * @Description: 更新后需要移除的文件
+ */
+module.exports = [
+  'lib/AlipayUnlocker.js',
+  'lib/Automator.js',
+  'lib/CommonFunctions.js',
+  'lib/FloatyUtil.js',
+  'lib/LockableStorage.js',
+  'lib/LogUtils.js',
+  'lib/RunningQueueDispatcher.js',
+  'lib/Timers.js',
+  'lib/TryRequestScreenCapture.js',
+  'lib/prototype/TryRequestScreenCapture.js',
+  'lib/WidgetUtils.js',
+  'extends/MuiltiTouchCollect-demo.js',
+  'lib/autojs-tools.dex',
+  'resources/for_update/autojs-tools.dex',
+  'config_with_webview.js',
+]
